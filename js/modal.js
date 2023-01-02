32802ee5ca69378e5a1c8ad78d8cf5dff653e960
Chaosborne <rick.chaosborne@gmail.com>
2023-01-02 21:42:23 +0300

    modal window

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+let modalWindow = document.querySelector(".modal-window");
+let overlay = document.querySelector(".overlay");
+let closeModalBtn = document.querySelector(".close-modal");
+let showModalBtns = document.querySelectorAll(".show-modal-btn");
+
+let openModal = () => {
+  modalWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+let closeModal = () => {
+  modalWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener("click", openModal);
+}
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modalWindow.classList.contains("hidden")) {
+    closeModal();
+  }
+});
