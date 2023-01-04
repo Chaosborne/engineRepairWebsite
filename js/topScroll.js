adb7c4b63b4366d073d1ed679fb0a03ed7074d4b
Chaosborne <rick.chaosborne@gmail.com>
2023-01-04 15:59:54 +0300

    top scroller and scroll behaviour

---
+++ js/topScroll.js
@@ -0,0 +1,9 @@
+let topScroller = document.getElementById('top-scroller');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 500) {
+    topScroller.classList.remove('hidden');
+  } else {
+    topScroller.classList.add('hidden');
+  }
+});
