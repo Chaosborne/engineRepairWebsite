4c641f26688314bffc12a6e0556a45f971c152d9
PozitronW <kolbasnikov@list.ru>
2023-01-05 19:12:37 +0300

    form

--- php/feedback.php
+++ php/feedback.php
@@ -1,4 +1,25 @@
+<?php
+    $mails = 'kolbasnikov@list.ru, rustemshagitov@gmail.com'
+    $url = $_POST['form--url']; // скрытое поле ботов
+    
 
-$name = $_POST['form--name'];
-$phone = $_POST['form--phone'];
-$message = $_POST['form--message'];
+    if ($url === NULL) {
+        if (isset($_POST['form--name'])) {
+            $name = $_POST['form--name'];
+        }
+        if (isset($_POST['form--phone'])) {
+            $phone = $_POST['form--phone'];
+        }
+        $message = $name . 'оставил заявку на обратный звонок<br>Телефон: ' . $phone;
+
+        $subject = 'Заявка с сайта: ' . $name;
+        if (isset($_POST['form--message'])) {
+            $message = $_POST['form--message'] . '<br>Телефон: ' . $phone;
+            $subject .= ' - запросил обрытный звонок';
+        }
+        
+        mail($mails, $tema, $message)
+
+    } else {
+        echo 'Спасибо, вы определены в петушиный угол';
+    }
