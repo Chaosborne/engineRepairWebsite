2ac9c5fba77cdaa94c5c0a550d23bf5c023c08a4
PozitronW <kolbasnikov@list.ru>
2023-01-05 20:41:23 +0300

    fix form

--- php/feedback.php
+++ php/feedback.php
@@ -1,13 +1,15 @@
 <?php
     $mails = 'kolbasnikov@list.ru, rustemshagitov@gmail.com';
     $url = $_POST['form--url']; // скрытое поле ботов
+    $headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
     
     if ($url == NULL) {
         if (isset($_POST['input--name'])) {
             $name = $_POST['input--name'];
             if (isset($_POST['input--phone'])) {
                 $phone = $_POST['input--phone'];
-                $message = $name . 'оставил заявку на обратный звонок<br>Телефон: ' . $phone;
+                $message = $name . ' оставил заявку на обратный звонок<br>Телефон: ' . $phone;
             }
         }
 
@@ -18,7 +20,7 @@
             $subject .= ' - запросил обрытный звонок';
         }
         var_dump($_POST['input--phone']);
-        mail($mails, $subject, $message);
+        mail($mails, $subject, $message, $headers);
         echo 'Спасибо' .$name. '. Ваша заявка отправлена';
 
     } else {
