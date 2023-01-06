b8a876bef22e7937ca63fcc49e1ab8bfaa52fb95
Chaosborne <rick.chaosborne@gmail.com>
2023-01-06 16:59:59 +0300

    update

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,17 @@
 let burgerBtn = document.querySelector('.burger-btn');
 let mobMenu = document.querySelector('.header__menu-list--burger');
+let burgerOverlay = document.querySelector('.burger-overlay');
 let burgerClosers = document.querySelectorAll('.burger-closer');
 
-burgerBtn.addEventListener('click', () => {
-  mobMenu.classList.toggle('header__menu-list--burger-shown');
-});
+let showToggler = () => {
+  mobMenu.classList.toggle('show-burger-menu');
+  burgerOverlay.classList.toggle('show-burger-overlay');
+};
+
+burgerBtn.addEventListener('click', showToggler);
 
 for (let i = 0; i < burgerClosers.length; i++) {
-  burgerClosers[i].addEventListener('click', () => {
-    mobMenu.classList.toggle('header__menu-list--burger-shown');
-  });
+  burgerClosers[i].addEventListener('click', showToggler);
 }
+
+burgerOverlay.addEventListener('click', showToggler);
