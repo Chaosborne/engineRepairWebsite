ba949781d7a0c0080f856d81534f4f4fb1ab1136
PozitronW <kolbasnikov@list.ru>
2023-01-10 18:12:27 +0300

    feedback update 4

--- php/feedback.php
+++ php/feedback.php
@@ -2,7 +2,10 @@
     include '../config.php';
     
     $mails = 'kolbasnikov@list.ru, rustemshagitov@gmail.com';
-    $url = $_POST['form--url']; // скрытое поле ботов
+    if (isset($_POST['form--url'])) {
+
+        $url = $_POST['form--url']; // скрытое поле для ботов
+    }
     $headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
     $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
     
