47464d0346cb346bb8126b28fc56f744ee78e3f8
PozitronW <kolbasnikov@list.ru>
2023-01-10 17:31:35 +0300

    include success

--- php/feedback.php
+++ php/feedback.php
@@ -27,6 +27,8 @@
 
             $phone_repl = str_replace('+7', '', $phone);
             echo 'Номер телефона +7' . $phone_repl;
+
+            include __DIR__ . '/sussess.php';
         }
 
     } else {
