0439c03cf20682fd095dd11bd7e74a1c0a430b3d
PozitronW <kolbasnikov@list.ru>
2023-01-10 17:28:48 +0300

    add mysql

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php 
+	$connection = new PDO("mysql:host=localhost;dbname=host1852358;charset=utf8", "host1852358_motordoctor", "ffadoxmIbvSUyuRI");
+ ?>
--- php/feedback.php
+++ php/feedback.php
@@ -1,4 +1,6 @@
 <?php
+    include __DIR__ . '/config.php';
+    
     $mails = 'kolbasnikov@list.ru, rustemshagitov@gmail.com';
     $url = $_POST['form--url']; // скрытое поле ботов
     $headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
@@ -11,17 +13,21 @@
                 $phone = $_POST['input--phone'];
                 $message = $name . ' оставил заявку на обратный звонок<br>Телефон: <a href="tel:'. $phone . '">' . $phone . '</a>';
             }
-        }
+    
+            $subject = 'Заявка с сайта: ' . $name; // Тема письма
+    
+            if (isset($_POST['textarea'])) {
+                $message = $_POST['textarea'] . '<br>Телефон: ' . $phone;
+                $subject .= ' - запросил обрытный звонок';
+            }
+            
+            mail($mails, $subject, $message, $headers);
+            echo 'Спасибо, ' .$name. '. Ваша заявка отправлена. <br>';
 
-        $subject = 'Заявка с сайта: ' . $name; // Тема письма
 
-        if (isset($_POST['textarea'])) {
-            $message = $_POST['textarea'] . '<br>Телефон: ' . $phone;
-            $subject .= ' - запросил обрытный звонок';
+            $phone_repl = str_replace('+7', '', $phone);
+            echo 'Номер телефона +7' . $phone_repl;
         }
-        
-        mail($mails, $subject, $message, $headers);
-        echo 'Спасибо, ' .$name. '. Ваша заявка отправлена';
 
     } else {
         echo 'Спасибо, вы определены в петушиный угол';
